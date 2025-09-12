c5cc710e75e29166301f071cd1f1fcb03f64dd8d
Jason Guo <jason.ghzr@gmail.com>
2025-09-11 23:11:13 -0700

    learn how to pass data, use navigation, styling, learned different kinds of navigators including nesting navigators

---
+++ section6-meals-app/screens/FavoritesScreen.js
@@ -0,0 +1,7 @@
+import { Text } from 'react-native-gesture-handler';
+
+function FavoritesScreen() {
+  return <Text>The favorites screen</Text>;
+}
+
+export default FavoritesScreen;
---
+++ section6-meals-app/screens/MealsOverviewScreen.js
@@ -0,0 +1,62 @@
+import MealItem from '../components/MealItem';
+import { MEALS, CATEGORIES } from '../data/dummy-data';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { useLayoutEffect } from 'react';
+
+function MealsOverviewScreen({ route, navigation }) {
+  const catId = route.params.categoryId;
+
+  const displayMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    function pressHandler() {
+      navigation.navigate('MealsDetail', {
+        mealId: item.id,
+      });
+    }
+
+    const mealItemProps = {
+      id: item.id,
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+      onPress: pressHandler,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
